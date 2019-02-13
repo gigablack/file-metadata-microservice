4b899ad87280e28e317025f4a8bcf9e2c8d467db
Gustavo Bethelmy <tenchu96@gmail.com>
2019-02-12 19:54:05 -0430

    fronted file name changed

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname,'./public')))
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'./index.html'))
+    res.sendFile(path.join(__dirname,'./page.html'))
 })
 
 app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
